6ccfca25cf8581764ff3c7a042e55d7323036c64
Normalhuman77 <20050224x@gmail.com>
2026-05-02 03:51:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
-const CACHE_NAME = 'minimal-reader-v1';
+const CACHE_NAME = 'reader-cache-v2';
 const ASSETS = [
   'index.html',
   'manifest.json'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
